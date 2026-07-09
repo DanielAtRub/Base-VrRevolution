1474b9a67d3cbeab83d4c5a2e7a9ed758edb208f
DanielAtRub <atienzarubidaniel@gmail.com>
2026-07-09 14:17:44 +0200

    Subida 17

---
+++ Assets/LoockAt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoockAt : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookVector = target.transform.position - transform.position;
+        lookVector.y = transform.position.y;
+        Quaternion rot = Quaternion.LookRotation(lookVector);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);
+    }
+}
